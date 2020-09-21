1a622f06e3a006c686d5df8c4a9a5c42fd712447
Sven Greb <development@svengreb.de>
2020-09-21 21:39:32 +0200

    nib API v0
    Implemented the API v0 with the following interfaces and types:
    
    - [I] `Nib` — a log-level based line printer for human-facing messages.
    - [T] `Verbosity` — defines the verbosity level of the line printer.
    
    GH-2

---
+++ nib.go
@@ -0,0 +1,15 @@
+// Copyright (c) 2020-present Sven Greb <development@svengreb.de>
+// This source code is licensed under the MIT license found in the LICENSE file.
+
+// Package nib provides log-level based line printer for human-facing Go CLI applications.
+package nib
+
+// Nib is a log-level based line printer for human-facing messages.
+type Nib interface {
+	Debugf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+	Fatalf(format string, args ...interface{})
+	Infof(format string, args ...interface{})
+	Successf(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+}
